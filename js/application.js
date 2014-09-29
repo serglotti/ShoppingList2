a38cc7d443f647f2f3e266690b57f4bb05445e93
serglotti <sergio.lottimore@gmail.com>
2014-09-29 13:28:03 -0300

    final_project

---
+++ js/application.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	//Add item scripting
+	$(".add-btn").on("mousedown", function() {
+		$(".cart-add").hide();
+		$(".cart-add-buttonDown").show();
+	})
+	
+	.on("mouseup", function() {
+		$(".cart-add-buttonDown").hide();
+		$(".cart-add").show();
+
+		// Adds item to list and appends delete and done buttons
+		var itemVal = document.getElementById("input-box-text").value;
+		var shopItem = $("<li class=listItem><span class=delete></span>".concat(itemVal).concat("<span class=done></span></li>"));
+		$(".item-list").append(shopItem);
+	})
+
+	// Deletes item from the list
+	$(".item-list").on("click", ".delete", function(){
+    	$(this).closest('li').fadeOut(250); //could also use remove()
+	})
+
+	// Checks off a completed list item
+	$(".item-list").on("click", ".done", function(){
+    	$(this).closest('li').addClass("strikethrough");
+	})
+
+});
